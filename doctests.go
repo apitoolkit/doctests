6affb2316238ce4c4e8ff172fffbfd5f44017741
Anthony Alaribe <anthonyalaribe@gmail.com>
2022-06-21 14:03:20 +0200

    savepoint

--- doctests.go
+++ doctests.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	// "go/build"
 	"go/doc"
 	"go/parser"
 	"go/token"
@@ -9,7 +10,10 @@ import (
 	"strings"
 
 	"github.com/kr/pretty"
+	// "github.com/switchupcb/yaegi/interp"
+	// "github.com/switchupcb/yaegi/stdlib"
 	"github.com/traefik/yaegi/interp"
+	"github.com/traefik/yaegi/stdlib"
 )
 
 // >>> AddTest
@@ -21,15 +25,15 @@ func AddTest() int {
 // ParseComments xxx
 func ParseComments() {
 	fset := token.NewFileSet() // positions are relative to fset
-	d, err := parser.ParseDir(fset, "./", nil, parser.ParseComments)
+	d, err := parser.ParseDir(fset, "../doctester/", nil, parser.ParseComments)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	for k, f := range d {
+	for k, packageNode := range d {
 		fmt.Println("package", k)
-		p := doc.New(f, "./", 2)
+		p := doc.New(packageNode, "../doctester/", 2)
 		// p.Filter(doc.Filter(func(s string) bool {
 		// 	fmt.Println("🔥", s)
 		// 	return strings.HasPrefix(s, ">>>")
@@ -63,17 +67,53 @@ func ParseComments() {
 					cmd := strings.TrimPrefix(">>>", v)
 					_ = cmd
 					// f.Dec
-					fs := os.DirFS("./")
+					fs := os.DirFS("../doctester/")
+					// cacheDir, err := os.UserCacheDir()
+					// if err != nil {
+					// 	fmt.Println("cacheDir", err)
+					// }
+
 					intp := interp.New(interp.Options{
+						GoPath: os.Getenv("GOPATH"),
+						// GoCache: cacheDir,
+						// GoPath:               "/Users/tonyalaribe/go/",
 						SourcecodeFilesystem: fs,
-						Unrestricted:         true,
+						// GoToolDir:            build.ToolDir,
+						// Stdout:               &stdout, Stderr: &stderr,
+						// Unrestricted:         true,
 					})
 					if err != nil {
 						fmt.Println(err)
 					}
 
+					intp.Use(stdlib.Symbols)
+
 					_ = intp
-					intp
+					intp.ImportUsed()
+
+					// prog, err := intp.CompileAST(packageNode)
+					// if err != nil {
+					// 	fmt.Println("error compile AST ", err)
+					// }
+					// _ = prog
+
+					// r, err := intp.Eval("fmt.Println(\"hello world \")")
+					r, err := intp.Eval("fmt.Println(\"hello world \")")
+					if err != nil {
+						fmt.Println("error ", err)
+					}
+					pretty.Println("RESP bla bla", r)
+
+					_, err = intp.EvalPath("./")
+					if err != nil {
+						fmt.Println("error ", err)
+					}
+
+					r, err = intp.Eval("AddTest()")
+					if err != nil {
+						fmt.Println("error ", err)
+					}
+					_ = r
 
 				}
 			}
--- go.mod
+++ go.mod
@@ -7,5 +7,10 @@ require github.com/kr/pretty v0.3.0
 require (
 	github.com/kr/text v0.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.6.1 // indirect
+	github.com/switchupcb/yaegi v0.10.2 // indirect
 	github.com/traefik/yaegi v0.13.0 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
+	golang.org/x/tools v0.1.11 // indirect
+	golang.org/x/xerrors v0.0.0-20220609144429-65e65417b02f // indirect
 )
